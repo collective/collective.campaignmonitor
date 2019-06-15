a33b110b4bbe8de322e96d32784144c4546ce219
Mikel Larreategi <mlarreategi@codesyntax.com>
2019-06-15 12:39:09 +0200

    we do not need plone.restapi

--- src/collective/campaignmonitor/testing.py
+++ src/collective/campaignmonitor/testing.py
@@ -18,8 +18,6 @@ class CollectiveCampaignmonitorLayer(PloneSandboxLayer):
         # Load any other ZCML that is required for your tests.
         # The z3c.autoinclude feature is disabled in the Plone fixture base
         # layer.
-        import plone.restapi
-        self.loadZCML(package=plone.restapi)
         self.loadZCML(package=collective.campaignmonitor)
 
     def setUpPloneSite(self, portal):
