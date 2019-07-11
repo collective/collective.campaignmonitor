64cd42eedac9994eec113d9975ae4c646f7a8a2f
Mikel Larreategi <mlarreategi@codesyntax.com>
2019-07-11 08:51:19 +0200

    Back to development: 1.0b2
    [ci skip]

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = '\n\n'.join(
 
 setup(
     name='collective.campaignmonitor',
-    version='1.0b1',
+    version='1.0b2.dev0',
     description="CampaingMonitor integration for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
