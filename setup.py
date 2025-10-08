cc907b651fa13987a0b160dc831b886cb575f2cc
Mikel Larreategi <mlarreategi@codesyntax.com>
2025-10-08 22:06:33 +0200

    internal: allow python 3.9 and Plone 6

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.10",
+    python_requires=">=3.9",
     install_requires=[
         "setuptools",
         # -*- Extra requirements: -*-
